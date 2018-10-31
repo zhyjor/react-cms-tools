cd8ed4f4d96c5ca00967d7e10fbdf79c05c4ce7a
zhyjor <zhyjor@163.com>
2018-10-31 17:53:27 +0800

    add:完成9-3

--- src/axios/index.js
+++ src/axios/index.js
@@ -1,6 +1,7 @@
 import JsonP from 'jsonp'
-
-export default class Axios{
+import axios from 'axios'
+import { Modal } from 'antd'
+export default class Axios {
     static jsonp(options) {
         return new Promise((resolve, reject) => {
             JsonP(options.url, {
@@ -13,5 +14,41 @@ export default class Axios{
                 }
             })
         })
+    }
+
+    static ajax(options){
+        let loading;
+        if (options.data && options.data.isShowLoading !== false){
+            loading = document.getElementById('ajaxLoading');
+            loading.style.display = 'block';
+        }
+        let baseApi = 'https://www.easy-mock.com/mock/5a7278e28d0c633b9c4adbd7/api';
+        return new Promise((resolve,reject)=>{
+            axios({
+                url:options.url,
+                method:'get',
+                baseURL:baseApi,
+                timeout:5000,
+                params: (options.data && options.data.params) || ''
+            }).then((response)=>{
+                if (options.data && options.data.isShowLoading !== false) {
+                    loading = document.getElementById('ajaxLoading');
+                    loading.style.display = 'none';
+                }
+                if (Number(response.status) === 200){
+                    let res = response.data;
+                    if (Number(res.code) === 0){
+                        resolve(res);
+                    }else{
+                        Modal.info({
+                            title:"提示",
+                            content:res.msg
+                        })
+                    }
+                }else{
+                    reject(response.data);
+                }
+            })
+        });
     }
 }
--- src/router.js
+++ src/router.js
@@ -2,8 +2,23 @@ import React from "react";
 import { HashRouter, Route, Switch } from "react-router-dom";
 import App from "./App";
 import Admin from "./admin";
+import Common from "./common";
 import Login from "./pages/login";
 import Button from "./pages/ui/button";
+import Modal from "./pages/ui/modals";
+import Loading from "./pages/ui/loadings";
+import Notice from "./pages/ui/notice";
+import Message from "./pages/ui/messages";
+import Tab from "./pages/ui/tabs";
+import Gallery from "./pages/ui/gallery";
+import Carousel from "./pages/ui/carousel";
+import FormLogin from "./pages/form/login";
+import FormRegister from "./pages/form/register";
+import BasicTable from "./pages/table/basicTable";
+import HighTable from "./pages/table/highTable";
+import City from "./pages/city";
+import Order from "./pages/order";
+import OrderDetail from "./pages/order/detail";
 import NoMatch from "./pages/nomatch/NoMatch";
 
 export default class IRouter extends React.Component {
@@ -14,11 +29,37 @@ export default class IRouter extends React.Component {
           <Switch>
             <Route path="/login" component={Login} />
             <Route
+              path="/common"
+              render={() => (
+                <Common>
+                  <Route
+                    path="/common/order/detail/:orderId"
+                    component={OrderDetail}
+                  />
+                </Common>
+              )}
+            />
+            <Route
               path="/"
               render={() => (
                 <Admin>
-                  <Route path="/ui/buttons" component={Button} />
-                  <Route component={NoMatch} />
+                  <Switch>
+                    <Route path="/ui/buttons" component={Button} />
+                    <Route path="/ui/modals" component={Modal} />
+                    <Route path="/ui/loadings" component={Loading} />
+                    <Route path="/ui/notification" component={Notice} />
+                    <Route path="/ui/messages" component={Message} />
+                    <Route path="/ui/tabs" component={Tab} />
+                    <Route path="/ui/gallery" component={Gallery} />
+                    <Route path="/ui/carousel" component={Carousel} />
+                    <Route path="/form/login" component={FormLogin} />
+                    <Route path="/form/reg" component={FormRegister} />
+                    <Route path="/table/basic" component={BasicTable} />
+                    <Route path="/table/high" component={HighTable} />
+                    <Route path="/city" component={City} />
+                    <Route path="/order" component={Order} />
+                    <Route component={NoMatch} />
+                  </Switch>
                 </Admin>
               )}
             />
